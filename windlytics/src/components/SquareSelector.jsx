7a185ace90e155ec02716c5692c4402b8c225709
Jacob Waldner <jacob@glasoceanelectric.com>
2025-11-08 12:05:10 -0400

    bug fixes + removed redundant code

--- windlytics/src/components/SquareSelector.jsx
+++ windlytics/src/components/SquareSelector.jsx
@@ -5,6 +5,17 @@ import {
 } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
+const markerIcon = new L.Icon({
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
 export default function SquareSelector({ onSelect }) {
   const [firstCorner, setFirstCorner] = useState(null);
   const [tempMarker, setTempMarker] = useState(null);
